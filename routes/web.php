73ed6b8241aa4c834f17690e01a50aab33776f72
MargheritaMottana <167569793+MargheritaMottana@users.noreply.github.com>
2024-10-20 14:55:59 +0200

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+    
+    // return view('welcome', compact('firstName', 'lastName'));
+});
+
+Route::get('/chi-siamo', function () {
+    return view('subpages.about');
+});
+
+// Route::get(PERCORSO CON CUI ARRIVARE ALLA PAGINA, FUNZIONE DI CALLBACK CHE MI CREA LA RISPOSTA DA DARE ALL UTENTE)
